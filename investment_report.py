db8e6b5a44780ddeb6f8a9dfb74eba05211cdfd1
jeffinjk <jeffinkurian6@gmail.com>
2025-01-18 11:29:05 +0530

    investment_report

--- investment_report.py
+++ investment_report.py
@@ -4,5 +4,16 @@ start_invest=float(input("Enter the initial investment:"))
 annual_return=float(input("Enter the annual return:"))/100
 years=int(input("Enter the number of years:"))
 
+current_invest=start_invest
+
+for i in range(1,years+1):
+    #current_invest=current_invest+(current_invest*annual_return)
+    current_invest=current_invest*(1+annual_return)
+    print("Year",i,"investment:",current_invest)
+    
+print("The final value after", years ,"years is: ",current_invest)
+    
+    
+
 
 
