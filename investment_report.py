5acbd958ceffcfc8fa26eb73152bcfe5687b8bd2
jeffinjk <jeffinkurian6@gmail.com>
2025-01-18 10:49:35 +0530

    step1

---
+++ investment_report.py
@@ -0,0 +1,8 @@
+print("investment report")
+print("---------------------")
+start_invest=float(input("Enter the initial investment:"))
+annual_return=float(input("Enter the annual return:"))/100
+years=int(input("Enter the number of years:"))
+
+
+
