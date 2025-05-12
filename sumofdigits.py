bf98c16bfb88435c27c7c94cb02d6310187da0f9
jeffinjk <jeffinkurian6@gmail.com>
2025-05-12 09:33:53 +0530

    sod 2

--- sumofdigits.py
+++ sumofdigits.py
@@ -1,11 +1,11 @@
-def sum_of_digits(n):
+def sod(n):
     total=0
-    while(n>0):
-        total+= n%10
+    while n>0:
+        total+=n%10
         n=n//10
     return total
-
+        
 for num in range(100,1001):
-    if sum_of_digits(num) %9==0:
+    if sod(num)%9==0:
         print(num)
         
